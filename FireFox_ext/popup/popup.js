fc4b0c18d0ff816187da66f7c29b916d5455ef66
User <pachenkomih@gmail.com>
2023-12-16 00:44:14 +0200

    Changed browser_action to content_scripts in manifest.json

---
+++ FireFox_ext/popup/popup.js
@@ -0,0 +1,7 @@
+const general_button_id = "general-button";
+
+/**"browser_action": {
+    "default_icon": "/icons/png-adsBlocker-48.png",
+    "default_title": "YSNP",
+    "default_popup": "popup/popup.html"
+  }*/
