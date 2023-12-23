ca9ff4f7429209974e51a6535d224c62773583d3
User <pachenkomih@gmail.com>
2023-12-23 14:51:10 +0200

    Delete popup-files and add icons in manifest.json

--- FireFox_ext/popup/popup.js
+++
@@ -1,7 +0,0 @@
-const general_button_id = "general-button";
-
-/**"browser_action": {
-    "default_icon": "/icons/png-adsBlocker-48.png",
-    "default_title": "YSNP",
-    "default_popup": "popup/popup.html"
-  }*/
