d2ad4c3ccf673c9662ba660023871333721efdb8
User <pachenkomih@gmail.com>
2023-12-23 05:03:19 +0200

    Now program write finded ads in json-file and adsBlcoker.js get all ads from that json-file

--- FireFox_ext/adsBlocker/adsBlocker_logical.py
+++ FireFox_ext/adsBlocker/adsBlocker_logical.py
@@ -1,19 +1,70 @@
 from bs4 import BeautifulSoup
-import requests
 import re
+import json
 
-def block_ads(html):
-    print(html)
-    print("\n\n\n")
-    #soup = BeautifulSoup(getPage.text, "html.parser")
-    #for div in soup.select('.branding-wrapp'):
-    #    div.decompose()
-    
-    #print(soup.select('.branding-wrapp'))
+def block_ads_class(soup):
+    elements = soup.find_all(class_=re.compile(r'ads', flags=re.IGNORECASE))
+    return elements
 
-    #print(soup.find_all(id=re.compile("oa-360")))
-    #for div in soup.find_all(id=re.compile("oa-360")):
-    #    div.decompose()
-    
-    #print(soup.select('#oa-360-1702968155185_j8n6yg553'))
-    #return soup.prettify()
+def block_ads_id(soup):
+    elements = soup.find_all(id=re.compile(r'ads', flags=re.IGNORECASE))
+    return elements
+
+def block_ads_src(soup):
+    elements = soup.find_all(src=re.compile(r'ads', flags=re.IGNORECASE))
+    return elements
+
+def block_ads_href(soup):
+    elements = soup.find_all(href=re.compile(r'ads', flags=re.IGNORECASE))
+    return elements
+
+def block_ads_data(soup):
+    elements = soup.find_all(data=re.compile(r'banner', flags=re.IGNORECASE))
+    return elements
+
+async def makeJSONFromDict(html):
+    dictOnloadJSON = {}
+    soup = BeautifulSoup(html, "html.parser")
+    dictOnloadJSON['Class'] = await classLoadInDict(soup)
+    dictOnloadJSON['ID'] = await idLoadInDict(soup)
+    dictOnloadJSON['SRC'] = await srcLoadInDict(soup)
+    dictOnloadJSON['Href'] = await hrefLoadInDict(soup)
+    dictOnloadJSON['Data'] = await dataLoadInDict(soup)
+        
+    with open("findedAds.json", "w") as file:
+        json.dump(dictOnloadJSON, file)
+
+async def classLoadInDict(soup):
+    elements = block_ads_class(soup)
+    classAdDict = {}
+    for index, element in enumerate(elements):
+        classAdDict[index] = element['class']
+    return classAdDict
+
+async def idLoadInDict(soup):
+    elements = block_ads_id(soup)
+    idAdDict = {}
+    for index, element in enumerate(elements):
+        idAdDict[index] = element['id']
+    return idAdDict
+
+async def srcLoadInDict(soup):
+    elements = block_ads_src(soup)
+    srcAdDict = {}
+    for index, element in enumerate(elements):
+        srcAdDict[index] = element['src']
+    return srcAdDict
+
+async def hrefLoadInDict(soup):
+    elements = block_ads_href(soup)
+    hrefAdDict = {}
+    for index, element in enumerate(elements):
+        hrefAdDict[index] = element['href']
+    return hrefAdDict
+
+async def dataLoadInDict(soup):
+    elements = block_ads_data(soup)
+    dataAdDict = {}
+    for index, element in enumerate(elements):
+        dataAdDict[index] = element['data']
+    return dataAdDict
--- FireFox_ext/adsBlocker/adsBlocker_server.py
+++ FireFox_ext/adsBlocker/adsBlocker_server.py
@@ -1,13 +1,17 @@
 import asyncio
 import websockets
-from adsBlocker_logical import block_ads
+from adsBlocker_logical import makeJSONFromDict
 
 async def server(websocket, path):
-    html = await websocket.recv()
-    print(html)
-
+    try:
+        html = await websocket.recv()
+        await makeJSONFromDict(html)
+        await websocket.send('True')
+    except Exception as e:
+        print(f"Error: {e}")
 
 startServer = websockets.serve(server, "192.168.0.103", 8080, max_size=None)
 
+print("Server start...")
 asyncio.get_event_loop().run_until_complete(startServer)
 asyncio.get_event_loop().run_forever()
